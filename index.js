833f20ac0c124ded32aa39a51e5486bb7556d460
Kyle Alwyn <kjalwyn@gmail.com>
2016-03-30 14:49:53 -0700

    remove log

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var parser = require('body-parser');
 var app = express();
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'content-type');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://loopbacklab.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
 });
@@ -13,17 +13,10 @@ app.use(parser.json());
 
 
 app.post('/', function (req, res) {
-  var params = req.body;
-  sendgrid.send({
-    to: 'kyle@loopbacklab.com',
-    from: params.from,
-    fromname: params.name,
-    subject: 'New Potential Client: ' + params.name,
-    text: params.text
-  }, function(err, json) {
+  sendgrid.send(req.body, function(err, json) {
     console.log(err, json);
     // TODO: Send email receipt to sender saying thanks for contacting us, we'll get back shortly
-    if (err) { return res.status(400).send(err); }
+    if (err) { return res.status(400).send(err.toString()); }
     res.sendStatus(204);
   });
 });
