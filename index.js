96483ff9ec4fab63f702cdb98dfaf39c1df50c3f
Kyle Alwyn <kjalwyn@gmail.com>
2016-04-13 14:42:04 -0700

    use request body to send

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ var express = require('express');
 var parser = require('body-parser');
 
 var app = express();
+
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'content-type');
   res.setHeader('Access-Control-Allow-Origin', 'http://loopbacklab.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
 });
+
 app.use(parser.json());
 
-app.post('/api/email', function (req, res) {
+app.post('/api/mail/send', function (req, res) {
   sendgrid.send(req.body, function(err, json) {
     // TODO: Send email receipt to sender saying thanks for contacting us, we'll get back shortly
     if (err) { return res.status(400).send(err.toString()); }
@@ -19,5 +21,4 @@ app.post('/api/email', function (req, res) {
   });
 });
 
-console.log('Wassup World!');
 app.listen(9000);
